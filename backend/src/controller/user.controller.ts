6851f3af5586754a62f491b79a152ff2438581a8
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-08-05 22:21:35 +0530

    [Backend] feat: added a UserController class and initialized registerUser func

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,11 +1,11 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import UserServices from "../service/user.service";
+import userRepository from "../repository/user.repoitory";
 
 export default class UserController {
-    
-    static registerUser= async(req:Request,res:Response ) =>{
-        const {username, password} = req.body
+  static registerUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
 
-        
-    }
-
+    const userServices = new UserServices(new userRepository());
+  };
 }
