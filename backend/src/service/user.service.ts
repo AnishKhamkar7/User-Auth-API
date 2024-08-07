b0732b9a40fa300906be180c74268f8899afde77
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-08-07 22:57:53 +0530

    [Backend] feat: added checkExistingUser and createUser Service to handle

--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -1,9 +1,34 @@
-import userRepository from "../repository/user.repoitory";
+import userRepository from "../repository/user.repository";
+import { ApiError } from "../utils/errorHandler";
+import validateUser from "../utils/validation.utils";
 
 export default class UserServices {
   UserRepository: userRepository;
 
   constructor(UserRepository: userRepository) {
     this.UserRepository = UserRepository;
+  }
+
+  async checkExistingUser(email: string) {
+    const existingUser = await this.UserRepository.findByEmail(email);
+  }
+
+  async createUser({ email, password }: { email: string; password: string }) {
+    await this.checkExistingUser(email);
+
+    const isEmailValid = validateUser.emailValidation(email);
+    const isPasswordValid = validateUser.passwordValidation(password);
+
+    if (!isEmailValid) {
+      throw new ApiError.BadRequestError("Invalid Email ID");
+    }
+
+    if (!isPasswordValid) {
+      throw new ApiError.BadRequestError("Invalid Password");
+    }
+
+    const user = await this.UserRepository.createUser({ email, password });
+
+    return user;
   }
 }
