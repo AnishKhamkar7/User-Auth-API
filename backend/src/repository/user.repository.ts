c7eb07b06e3cb543ab1dcf2b49bfdab45f307088
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-08-07 22:56:46 +0530

    [Backend] feat: added findByEmail and creteUser repository to interact with the model

--- backend/src/repository/user.repository.ts
+++ backend/src/repository/user.repository.ts
@@ -1,11 +1,40 @@
 import { prisma } from "../db/user.db";
+import { ApiError } from "../utils/errorHandler";
 
 export default class userRepository {
   async findById(userId: string) {
-    return await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         id: userId,
       },
     });
+    if (!user) {
+      throw new ApiError.NotFoundError("User Not Found");
+    }
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new ApiError.NotFoundError("User Not Found");
+    }
+  }
+
+  async createUser({ email, password }: { email: string; password: string }) {
+    const createdUser = await prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+    if (!createdUser) {
+      throw new ApiError.InternalServerError("User Registration Failed");
+    }
+
+    return createdUser;
   }
 }
