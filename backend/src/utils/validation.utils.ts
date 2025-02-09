1d6d741e1038ca9c3053e8138378de044956f1f7
Anish <anishkhamkar7@gmail.com>
2025-02-09 17:03:16 +0530

    [Backend chore: added bcrypt to validate Password and compare password util function

--- backend/src/utils/validation.utils.ts
+++ backend/src/utils/validation.utils.ts
@@ -1,11 +1,17 @@
 import validator from "validator";
+import { ApiError } from "./errorHandler";
+import bcrypt from "bcrypt";
 
-export default class validateUser {
-  static emailValidation(email: string): boolean {
-    return validator.isEmail(email);
+export default class ValidateUser {
+  static emailValidation(email: string) {
+    const isEmailValid = validator.isEmail(email);
+
+    if (!isEmailValid) {
+      return new ApiError.BadRequestError("Invalid Email Format");
+    }
   }
 
-  static passwordValidation(password: string): boolean {
+  static passwordValidation(password: string) {
     const options = {
       minLength: 8,
       minLowercase: 1,
@@ -14,6 +20,25 @@ export default class validateUser {
       minSymbols: 1,
     };
 
-    return validator.isStrongPassword(password, options);
+    const isPasswordValid = validator.isStrongPassword(password, options);
+
+    if (isPasswordValid) {
+      return new ApiError.BadRequestError("Invalid Password Format");
+    }
+  }
+  static async passwordEncrypt(password: string): Promise<string> {
+    try {
+      return await bcrypt.hash(password, 10);
+    } catch (error) {
+      throw new ApiError.InternalServerError("Error hashing password");
+    }
+  }
+
+  static async passwordVerify(newPassword: string, hashedPassword: string): Promise<boolean> {
+    try {
+      return await bcrypt.compare(newPassword, hashedPassword);
+    } catch (error) {
+      throw new ApiError.InternalServerError("Error verifying password");
+    }
   }
 }
