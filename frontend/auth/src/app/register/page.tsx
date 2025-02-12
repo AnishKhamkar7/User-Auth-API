afa67d96a74b04df90c363994b3d68b29bef6c7c
Anish <anishkhamkar7@gmail.com>
2025-02-12 23:45:58 +0530

    [Frontend] refactor: UI fixes

--- frontend/auth/src/app/register/page.tsx
+++ frontend/auth/src/app/register/page.tsx
@@ -2,7 +2,6 @@
 
 import {
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -13,6 +12,8 @@ import { useForm } from "react-hook-form";
 import { Form } from "@/components/ui/form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Mail, User, Lock } from "lucide-react";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -33,18 +34,24 @@ const Register = () => {
   const onSubmit = (values: z.infer<typeof formSchema>) => {};
 
   return (
-    <div className=" bg-gray-200 p-6 rounded-lg max-w-md mx-auto mt-20">
+    <div className=" bg-gray-200 p-6 rounded-lg max-w-md mx-auto mt-20 items-center">
+      <h3 className="text-2xl mb-4 text-center">Register</h3>
+
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>UserName</FormLabel>
-                <FormDescription>Enter your Username</FormDescription>
+
                 <FormControl>
-                  <Input placeholder="John Doe" {...field} />
+                  <Input
+                    placeholder="John Doe"
+                    className="hover:border-gray-400 transition-all duration-300 cursor-pointer"
+                    {...field}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -55,11 +62,18 @@ const Register = () => {
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>UserName</FormLabel>
+                <FormLabel className="flex gap-3 items-center">
+                  <Mail />
+                  Email
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder="JohnDoe@123.com" {...field} />
+                  <Input
+                    placeholder="JohnDoe@123.com"
+                    className="hover:border-gray-400 transition-all duration-300 cursor-pointer"
+                    {...field}
+                  />
                 </FormControl>
-                <FormDescription>Enter your Email</FormDescription>
+
                 <FormMessage />
               </FormItem>
             )}
@@ -69,15 +83,25 @@ const Register = () => {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>UserName</FormLabel>
+                <FormLabel className="flex gap-3 items-center">
+                  <Lock />
+                  Password
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder="Don't see me" {...field} />
+                  <Input
+                    placeholder="Don't see me"
+                    className="hover:border-gray-400 transition-all duration-300 cursor-pointer"
+                    {...field}
+                  />
                 </FormControl>
-                <FormDescription>Enter your Username</FormDescription>
+
                 <FormMessage />
               </FormItem>
             )}
           />
+          <div className="flex justify-start">
+            <Button type="submit">Submit</Button>
+          </div>
         </form>
       </Form>
     </div>
